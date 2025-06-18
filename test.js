8bc9f465e9ad0838f0ae21bd0d6652df50e96b74
SUDIPSHIT777 <sudipshit777@gmail.com>
2025-06-18 13:23:14 +0530

    Number Guess

---
+++ test.js
@@ -0,0 +1,30 @@
+const min = 1, max = 50;
+let attems = 0
+let randomenum = Math.floor(Math.random() * max) + min
+// alert(`Enter Number ${min} to ${max}`)
+function gess() {
+    let val = Number(document.getElementById("val").value);
+    if (isNaN(val)) {
+        alert("Enter a valid Number");
+    }
+    else if (val < min || val > max) {
+        alert("Value Not Should be In Range so Enter a valid Number");
+    }
+    else {
+        attems++;
+        if (val < randomenum) {
+            alert("Value Is To Low ! TRY AGAIN")
+        }
+        else if (val > randomenum) {
+            alert("Value Is To High ! TRY AGAIN")
+        }
+        else {
+            document.getElementById("demo").textContent = `You Won 🎉🎉✨🎊 The Number is   ${val}`
+
+        }
+    }
+}
+
+function reset() {
+    location.reload(true);
+}
