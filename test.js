299713219b8440d011fc290f00190bed02d8615e
SUDIPSHIT777 <sudipshit777@gmail.com>
2025-06-19 10:01:25 +0530

    test.js

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 const min = 1, max = 50;
 let attems = 0
 let randomenum = Math.floor(Math.random() * max) + min
+let val;
 // alert(`Enter Number ${min} to ${max}`)
 function gess() {
-    let val = Number(document.getElementById("val").value);
+    val = Number(document.getElementById("val").value);
+    console.log(val)
     if (isNaN(val)) {
         alert("Enter a valid Number");
     }
@@ -19,13 +21,12 @@ function gess() {
             alert("Value Is To High ! TRY AGAIN")
         }
         else {
-             
-            document.getElementById("demo").innerHTML = `You Won 🎉🎉✨🎊 The Number is   ${val} <br> <p style = "color: white">You Attend ${attems} Times</p>`
-            attems++;
+            document.getElementById("demo").innerHTML = `You Won 🎉🎉✨🎊 The Number is   ${val} <br> <p style = "color: white">You Attend ${attems} Times</p>`;
         }
     }
 }
 
 function reset() {
-    location.reload(true);
+    val = document.getElementById("val");
+    val.value = ""
 }
